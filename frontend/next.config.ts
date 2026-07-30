1c7761baf56671ce04dc64a471f24741cc5e0287
SameerAkram1 <sameerakram101@gmail.com>
2026-07-30 12:47:18 +0500

    Fix home page

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,8 +5,8 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
-        source: '/landing',
-        destination: '/landing/index.html',
+        source: '/',
+        destination: '/index.html',
       },
     ];
   },
